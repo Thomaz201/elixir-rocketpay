b62a2ffa8eb09f55094f5a522d75c545e15719f2
Thomaz201 <lucasthomazr@hotmail.com>
2021-02-27 17:40:17 -0300

    Accounts creation implemented

--- lib/rocketpay_web/views/users_view.ex
+++ lib/rocketpay_web/views/users_view.ex
@@ -1,14 +1,26 @@
 defmodule RocketpayWeb.UsersView do
-  alias Rocketpay.User
+  alias Rocketpay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname, email: email}}) do
+  def render("create.json", %{
+        user: %User{
+          account: %Account{id: account_id, balance: balance},
+          id: id,
+          name: name,
+          nickname: nickname,
+          email: email
+        }
+      }) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
         nickname: nickname,
-        email: email
+        email: email,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
